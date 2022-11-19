4421bd7615dc559acf71a7e4355dbfa9da027167
Ravi Prakash <ravip140891@gmail.com>
2022-11-19 15:57:51 +0530

    done

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const nameEl = document.querySelector("#name");
 const emailEl = document.querySelector("#email");
 const phoneEl = document.querySelector("#number");
 const tableEl = document.querySelector(".table");
+const alert = document.querySelector(".red");
+
 
 function createTable(e){
     e.preventDefault();
     const newName = nameEl.value;
     const newEmail = emailEl.value;
     const newPhone = phoneEl.value;
+    if(newEmail == ""){
+        let warning = setTimeout(() => {
+    alert.style.display = "block";
+    },100);
+    } else{
     tbodyEl.innerHTML += `
                 <tr>
                 <td class="name-td">${newName}</td>
@@ -20,7 +27,9 @@ function createTable(e){
 
     nameEl.value = "";
     emailEl.value = "";
-    phoneEl.value = "";       
+    phoneEl.value = "";  
+    alert.style.display = "none";    
+}
 }
 
 function deleteRow(e){
